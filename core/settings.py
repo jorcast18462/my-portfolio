62cef992d7bfc36de18fe1f8f6432a903eed1777
Omarreda180 <omar90180360@gmail.com>
2022-01-27 01:09:00 +0200

    Last Push i think!

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,13 +74,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dpceviqgmurmo',
+        'USER' : 'rgtvbmtiwrzhcq',
+        'PASSWORD' : '9370962331460c0b4f7d98cd0b1b553e94fe58ae7d9d87c7c139d9756f29517d',
+        'HOST' : 'ec2-44-199-83-229.compute-1.amazonaws.com',
+        'PORT' : '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -129,6 +141,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'core/static'
 ]
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+# STATIC_URL = '/static/'
 
 # MEDIA
 MEDIA_URL = '/media/'
